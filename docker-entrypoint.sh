ada27de0da5d2b42536859c40744a85e5084a824
malanto <78157677+malanto@users.noreply.github.com>
2025-09-05 14:35:09 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,5 +8,7 @@ fi
 # Execute postinstall to auto-populate config.yaml with missing values
 npm run postinstall
 
+cp ./launch.sh ./history/launch.sh
+
 ./history/launch.sh env && ./history/launch.sh init
 exec node server.js --listen "$@"
